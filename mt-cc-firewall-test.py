71cdaf322924b9fd4d83117885035e91d6c5027f
Thomas Kerley <kerleytk@hotmail.com>
2023-05-04 19:56:46 +0100

    Update mt-cc-firewall-test.py

--- mt-cc-firewall-test.py
+++ mt-cc-firewall-test.py
@@ -1,6 +1,10 @@
-import PySimpleGUI as sg
 import socket
+from pythonping import ping
 #
+host = input("What server do you want to test? " )
+ip_addr = socket.gethostbyname(host)
+
+# ports used for testing callswitch
 port_80=80
 port_443=443
 port_5060=5060
@@ -10,9 +14,7 @@ port_10005=10005
 port_10007=10007
 port_10009=10009
 port_11389=11389
-
 # turn ports into a list
-
 ports = [
     port_80,
     port_443,
@@ -25,56 +27,45 @@ ports = [
     port_11389
     ]
 
-# UI Design
-sg.theme('BlueMono')
-
-gui_title = sg.Text('Welcome to the TelcoSwitch CC / MT server firewall tester'),
-gui_host = sg.Text('What server do you want to test?'), sg.InputText(key="gui_input"),
-gui_output = sg.Output(size=(80, 30)),
-gui_button = sg.Button('Ok'), sg.Button('Close')
-			
-layout = [
-    [gui_title],
-    [gui_host],
-    [gui_output],
-    [gui_button]
-    ]
-
-# Create the Window
-window = sg.Window('TelcoSwitch Callswitch Firewall Tester', layout)
-
-# Event Loop to process "events" and get the "values" of the inputs
-
-while True:
-    
-    event, values = window.read()
-    host = str(values["gui_input"])
-    
-    if event == sg.WIN_CLOSED or event == 'Close': # if user closes window or clicks the close button
-        break
-        exit()
-        
 # Port tester if the user continues
     
-    ip_addr = socket.gethostbyname(host)
+ip_addr = socket.gethostbyname(host)
+
+print(f"The server you are testing is: {host}\n")
+print (f"This server is hosted on IP address: {ip_addr}\n")
+#packet loss and latacny test
+print ("\nWe are testing packet loss\n")
+
+action = ping (host, count = 10, timeout =2)
+if action.packet_loss == 0.0:
+    print ("Your network is good\n")
+else:
+    print ("You have a packet loss issue\n")
+
+print ("\nWe are testing for network latancy\n")
+print ("Average Latancy (ms): ")
+print (action.rtt_avg_ms)
+print ("Max Latancy (ms): ")
+print (action.rtt_max_ms)
+
+if action.rtt_max_ms <= 150.0:
+    print ("Your network is good\n")
+else:
+    print ("You have a latacny issue\n")
+
+for port in range(len(ports)):
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(1)
+    result = sock.connect_ex((host,int(ports[port])))
     
-    print(f"The server you are testing is: {host}\n")
-    print (f"This server is hosted on IP address: {ip_addr}\n")
+    print (f"Testing port {ports[port]} - on server {host}")
 
-    for port in range(len(ports)):
+    if result == 0:
+        print("Host: {}, Port: {} - Enabled and working".format(host, ports[port]))
+    else:
+        print("Host: {}, - Firewall Issue on port: {} \nYou will need to address this".format(host, ports[port]))
+        break
+    sock.close()
 
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(1)
-        result = sock.connect_ex((host,int(ports[port])))
-    
-        print (f"Testing port {ports[port]} - on server {host}")
-
-        if result == 0:
-            print("Host: {}, Port: {} - Enabled and working\n".format(host, ports[port]))
-        else:
-            print("Host: {}, - Firewall Issue on port: {} \nYou will need to address this".format(host, ports[port]))
-        sock.close()
-
-    print ("\nPlease visit: https://support.telcoswitch.com/hc/en-us/articles/207279309-Network-ports-used-by-CallSwitch-Firewall-Guide- for more info\n")
-
-window.close()
+print ("\n\nPlease visit: https://support.telcoswitch.com/hc/en-us/articles/207279309-Network-ports-used-by-CallSwitch-Firewall-Guide- for more info\n")
