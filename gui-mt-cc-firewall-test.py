758cb2be6599ad6d45572e9149028577187d53bb
Thomas Kerley <kerleytk@hotmail.com>
2023-05-04 19:32:41 +0100

    Update gui-mt-cc-firewall-test.py

--- gui-mt-cc-firewall-test.py
+++ gui-mt-cc-firewall-test.py
@@ -1,5 +1,8 @@
 import PySimpleGUI as sg
 import socket
+import webbrowser
+from pythonping import ping
+
 #
 port_80=80
 port_443=443
@@ -29,7 +32,7 @@ sg.theme('BlueMono')
 gui_title = sg.Text('Welcome to the TelcoSwitch CC / MT server firewall tester'),
 gui_host = sg.Text('What server do you want to test?'), sg.InputText(key="gui_input"),
 gui_output = sg.Output(size=(80, 20)),
-gui_button = sg.Button('Ok'), sg.Button('Cancel')
+gui_button = sg.Button('Firewall test'), sg.Button('Packet loss test'),sg.Button('Latancy test'),sg.Button('Help'), sg.Button('Cancel')
 			
 layout = [
     [gui_title],
@@ -39,7 +42,7 @@ layout = [
     ]
 
 # Create the Window
-window = sg.Window('TelcoSwitch Callswitch Firewall Tester', layout)
+window = sg.Window('TelcoSwitch Callswitch Firewall Tester',icon="icon.png").Layout(layout)
 
 # Event Loop to process "events" and get the "values" of the inputs
 
@@ -51,25 +54,55 @@ while True:
     if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
         break
         exit()
-
-    ip_addr = socket.gethostbyname(host)
-    print(f"The server you are testing is: {host}\n")
-    print (f"This server is hosted on IP address: {ip_addr}\n")
-
-    for port in range(len(ports)):
-
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(1)
-        result = sock.connect_ex((host,int(ports[port])))
     
-        print (f"Testing port {ports[port]} - on server {host}")
+    if event == event == "Packet loss test":
+        
+        print ("\nWe are testing packet loss\n")
 
-        if result == 0:
-            print("Host: {}, Port: {} - Enabled and working\n".format(host, ports[port]))
+        action = ping (host, count = 10, timeout =2)
+        if action.packet_loss == 0.0:
+            print ("Your network is good")
         else:
-            print("Host: {}, - Firewall Issue on port: {} \nYou will need to address this".format(host, ports[port]))
-        sock.close()
+            print ("You have a packet loss issue")
 
-    print ("\nPlease visit: https://support.telcoswitch.com/hc/en-us/articles/207279309-Network-ports-used-by-CallSwitch-Firewall-Guide- for more info\n")
+    if event == event == "Latancy test":
+
+        print ("\nWe are for network latancy\n")
+        
+        action = ping(target=host, count=10, timeout=2)
+
+        print ("Average Latancy (ms): ")
+        print (action.rtt_avg_ms)
+        print ("Max Latancy (ms): ")
+        print (action.rtt_max_ms)
+
+        if action.rtt_max_ms <= 150.0:
+            print ("Your network is good")
+        else:
+            print ("You have a latacny issue")
+
+    if event == event == "Firewall test":
+        ip_addr = socket.gethostbyname(host)
+        print(f"\nThe server you are testing is: {host}\n")
+        print (f"This server is hosted on IP address: {ip_addr}\n")
+
+        for port in range(len(ports)):
+
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(1)
+            result = sock.connect_ex((host,int(ports[port])))
+        
+            print (f"Testing port {ports[port]} - on server {host}")
+
+            if result == 0:
+                print("Host: {}, Port: {} - Enabled and working\n".format(host, ports[port]))
+            else:
+                print("Host: {}, - Firewall Issue on port: {} \nYou will need to address this".format(host, ports[port]))
+            sock.close()
+
+        print ("\nPlease visit: https://support.telcoswitch.com/hc/en-us/articles/207279309-Network-ports-used-by-CallSwitch-Firewall-Guide- for more info\n")
+
+    if event == event == "Help":
+        webbrowser.open ("https://support.telcoswitch.com/hc/en-us/articles/207279309-Network-ports-used-by-CallSwitch-Firewall-Guide-") 
 
 window.close()
